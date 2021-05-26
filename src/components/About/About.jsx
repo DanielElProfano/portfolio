6046af1cd2ff194a66a30c69b64295f043611e2a
danielElProfano <daniel.gonzalez.guijarro@gmail.com>
2021-05-26 22:24:20 +0200

    add links on footer

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,20 +4,16 @@ const About = () => {
     return(
         <div>
             <section className="b-about__section">
+                <div className="b-about__img">
+                    <img className="b-about__img-container" src="./assets/images/londres.jpg" alt="Daniel" />
+                </div>
                 <div className="b-about__title">
-                    
                     <h1 className="b-about__h1">Daniel González</h1>
                     <h3 className="b-about__h3">FullStack web developer </h3>
                     <h3 className="b-about__h3">Trust, hardworker, empathy.</h3>
-
-                </div>
-                    <div className="b-about__img">
-                        <img className="b-about__img-container" src="./assets/images/londres.jpg" alt="Daniel" />
-                        {/* <div className="b-about__circle"> </div> */}
                 </div>
             </section>
             <section className="b-about__section b-about__section--2">
-                
                 <div className="b-about__image b-about__image--2">
                     <div className="b-about__img b-about__img--2"></div>
                     <div className="b-about__circle b-about__circle--2"></div>
@@ -25,19 +21,19 @@ const About = () => {
                 </div>
                 <div className="b-about__title b-about__title--2">
                     <h1 className="b-about__h1">Tecnologías</h1>
-                    <h1 className="b-about__h1 b-about__h1--red">Front</h1>
+                    <h1 className="b-about__h1 b-about__h1--color">Front</h1>
                     <div  className="b-about__comentarios">
-                        <p className="b-about__text b-about__text--big b-about__text--red">React</p>
-                        <p className="b-about__text b-about__text--big b-about__text--red">Angular</p>
+                        <p className="b-about__text b-about__text--big b-about__text--color">React</p>
+                        <p className="b-about__text b-about__text--big b-about__text--color">Angular</p>
+                        <p className="b-about__text b-about__text--big b-about__text--color">Javascript</p>
                     </div>  
-                    <h1 className="b-about__h1 b-about__h1--red">Back</h1>
+                    <h1 className="b-about__h1 b-about__h1--color">Back</h1>
                     <div  className="b-about__comentarios">
-                        <p className="b-about__text b-about__text--big b-about__text--red">Node JS</p>
-                        <p className="b-about__text b-about__text--big b-about__text--red">PHP</p>
+                        <p className="b-about__text b-about__text--big b-about__text--color">Node JS</p>
+                        <p className="b-about__text b-about__text--big b-about__text--color">PHP</p>
                     </div>  
                 </div>           
-        </section>
-
+            </section>
         </div>
     )
 } 
--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -2,9 +2,24 @@ import './Footer.scss';
 
 const Footer = () => {
     return(
-        <div>
-            Footer
+        <footer class="b-footer">
+        <div class="b-footer_container">
+          <div className="b-footer__links">
+              <div className="b-footer__description">
+                  <span>Front</span>
+                  <a href="https://github.com/DanielElProfano/academia-react">
+                      <img className="b-footer__icon" src="/assets/icons/github.png" target="_blank" alt="github icon"></img>
+                  </a>
+              </div>
+              <div className="b-footer__description">
+                  linkedin
+                  <a href="https://www.linkedin.com/in/daniel-gonzalez-guijarro">
+                      <img className="b-footer__icon" src="./assets/icons/linkedin.png" target="_blank" alt="github icon"></img>
+                  </a>
+              </div>
+              </div>
         </div>
+      </footer>
     )
 };
 
